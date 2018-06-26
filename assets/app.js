9f35e92c5db0f0784f6b10f07544968028dcb11b
Ash <asha.mohamed.28@gmail.com>
2018-06-25 21:41:59 -0400

    gifs load onto page when btns are clicked

--- assets/app.js
+++ assets/app.js
@@ -1,68 +1,85 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-// 1.CREATE ARRAY OF STRINGS
+    // 1.CREATE ARRAY OF STRINGS
 
-var topics =["cat", "dog", "hamster", "birds"]; 
-    
-// 2. CREATE A FUCTION TO MAKE THE BUTTONS 
-function createBtns() {
+    var topics = ["cat", "dog", "hamster", "birds"];
 
-for (var i =0; i<topics.length; i++){
+    // 2. CREATE A FUCTION TO MAKE THE BUTTONS 
+    function createBtns() {
 
-    var a = $("<button>"); 
-    a.addClass("topics"); // limits 'scope' of document
-    a.addClass("btn");         
-    a.attr("data-name", topics[i]); 
-    a.text(topics[i]);
-    $("#btns").append(a); 
-    
-}
+        for (var i = 0; i < topics.length; i++) {
 
-}
-createBtns(); 
+            var a = $("<button class='btn-primary'>");
+            a.addClass("topics"); // limits 'scope' of document
+            a.addClass("btn");
+            a.attr("data-name", topics[i]);
+            a.text(topics[i]);
+            $("#btns").append(a);
+        }
+    }
+    createBtns();
 
-// 3. CLICK EVENT --> 10 STATIC IMAGES 
+    // 3. CLICK EVENT --> 10 STATIC IMAGES 
 
     //B. FILL IMAGES --> AJAX 
+    function getGifs() {
+        $(document).on('click', ".topics", function () {
 
-function getGifs() {
-$(document).on('click', ".topics", function() {
+            //A. EMPTY DIV
+            $("#images").empty();
 
-    //A. EMPTY DIV
-$("#images").empty(); 
+            var topic = $(this).attr("data-name"); //copying the data-name
+            var apiKey = "SmyC6pLugsHSOmSjmIrnBRtwz8RW1opF";
+            var limit = 3; 
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + ".offset&api_key=" + apiKey + "&limit=" + limit;
 
-var topic = $(this).attr("data-name"); //copying the data-name
+            console.log(queryURL);
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + ".offset&api_key=dc6zaTOxFJmzC&limit=10";
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).done(function (response) {
+                var results = response.data;
+                console.log(results);
 
-$.ajax({
-    url: queryURL, 
-    method: 'GET'
-})
+                for (var i = 0; i < results.length; i++) {
 
-// state change 
-// look for attribute called data-state, still vs animated 
+                    var gifDiv = $("<div class='col-6'>")
+
+                    var rating = results[i].rating;
+                    var animatedImage = results[i].images.fixed_height.url;
+                    var staticImage = results[i].images.fixed_height_still.url;
+                    var p = $("<p>").text("Rating: " + rating);
+                    var imageDisplay = $("<img>"); 
+
+                    imageDisplay.attr("src", animatedImage); 
+                    imageDisplay.attr("data-state", "still"); 
+                    imageDisplay.attr("data-state", staticImage); 
+                    imageDisplay.attr("data-state", animatedImage); 
+
+                    $(gifDiv).append(p); 
+                    $(gifDiv).prepend(imageDisplay);
+
+                    $("#images").prepend(gifDiv);
+                }
+
+            });
+
+        });
+
+    }
 
 
-})
 
-}
-
-getGifs(); 
+    getGifs();
 
 
-// 4. CHANGE DATA STATE FROM STATIC TO ANIMATED 
-
+    // 4. CHANGE DATA STATE FROM STATIC TO ANIMATED 
+    // state change 
+    // look for attribute called data-state, still vs animated 
 
 
 
 
 
-
-
-
-
-
-
-}); 
-
+});
