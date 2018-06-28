35ce2cf82e39bb5f6abb62d6e55567c203c23d13
Ash <asha.mohamed.28@gmail.com>
2018-06-27 20:48:15 -0400

    text form added for user to add their own gif button

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,14 @@
 $(document).ready(function () {
 
     // 1.CREATE ARRAY OF STRINGS
-
-    var topics = ["cat", "dog", "hamster", "birds"];
+    var topics = ["jada pinkett", "beyonce", "tiffany haddish", "viola davis", "kerry washington", "angela bassett", "zendaya", "gabrielle union", "tracee ellis ross"];
 
     // 2. CREATE A FUCTION TO MAKE THE BUTTONS 
     function createBtns() {
 
         for (var i = 0; i < topics.length; i++) {
 
-            var a = $("<button class='btn-primary'>");
+            var a = $("<button class='btn-info btn-small m-2 col-3'>");
             a.addClass("topics"); // limits 'scope' of document
             a.addClass("btn");
             a.attr("data-name", topics[i]);
@@ -20,17 +19,14 @@ $(document).ready(function () {
     createBtns();
 
     // 3. CLICK EVENT --> 10 STATIC IMAGES 
-
-    //B. FILL IMAGES --> AJAX 
+    // A. FILL IMAGES --> AJAX 
     function getGifs() {
         $(document).on('click', ".topics", function () {
-
-            //A. EMPTY DIV
             $("#images").empty();
 
             var topic = $(this).attr("data-name"); //copying the data-name
             var apiKey = "SmyC6pLugsHSOmSjmIrnBRtwz8RW1opF";
-            var limit = 3;
+            var limit = 10;
             var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + ".offset&api_key=" + apiKey + "&limit=" + limit;
 
             console.log(queryURL);
@@ -42,15 +38,16 @@ $(document).ready(function () {
                 var results = response.data;
                 console.log(results);
 
+                // B. FOR LOOP TO DISPLAY IMAGES: ADDING DATA ATTRIBUTES 
                 for (var i = 0; i < results.length; i++) {
 
-                    var gifDiv = $("<div class='gifDiv' class='col-6'>")
+                    var gifDiv = $("<div class='col-6'>")
 
                     var rating = results[i].rating;
                     var animatedImageUrl = results[i].images.fixed_height.url;
                     var staticImageUrl = results[i].images.fixed_height_still.url;
-                    var p = $("<p>").text("Rating: " + rating);
-                    var imageDisplay = $("<img>");
+                    var p = $("<p class=' font-weight-bold '>").text("Rating: " + rating);
+                    var imageDisplay = $("<img class= ' flex-box rounded'>");
 
                     // For Still / Animated Gifs
                     imageDisplay.addClass("imageGiphy")
@@ -59,26 +56,19 @@ $(document).ready(function () {
                     imageDisplay.attr("data-still", staticImageUrl);
                     imageDisplay.attr("data-animate", animatedImageUrl);
 
-
                     $(gifDiv).append(p);
                     $(gifDiv).prepend(imageDisplay);
 
                     $("#images").prepend(gifDiv);
                 }
-
             });
-
         });
-
     }
-
     getGifs();
 
 
     // 4. CHANGE DATA STATE FROM STATIC TO ANIMATED 
-    // look for attribute called data-state, still vs animated 
-
-    // Click event will stop/start gifs 
+    // Image click event will stop/start gifs 
     $(document).on("click", ".imageGiphy", startStopGifs);
 
     function startStopGifs() {
@@ -91,16 +81,21 @@ $(document).ready(function () {
             $(this).attr("src", $(this).attr("data-still"));
             $(this).attr("data-state", "still");
         };
-
     }
-
-
 
     startStopGifs();
 
+    // 5. CREATE FORM FOR USER TO ADD BUTTONS  
+    //Submit button click event takes search term from form input, trims and pushes to topics array, displays button
 
+    $("#submit").on("click", function (event) {
+        event.preventDefault();
 
-
-
+        var searchTerm = $("#inputText").val().trim();
+        topics.push(searchTerm);
+        console.log(searchTerm);
+        $("#inputText").val('');
+        createBtns();
+    });
 
 });
